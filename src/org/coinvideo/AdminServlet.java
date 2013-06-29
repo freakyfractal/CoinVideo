09d86ae1e9993743aaca952c6da668cced273d92
coinpr0n <coinpr0n@gmail.com>
2013-06-29 17:39:03 +0200

    cleaned up AdminServlet.java

--- src/org/coinvideo/AdminServlet.java
+++ src/org/coinvideo/AdminServlet.java
@@ -19,7 +19,7 @@ public class AdminServlet extends HttpServlet {
 	private static final Logger _logger = Logger.getLogger(AdminServlet.class.getName());
 	
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		_logger.info("[WITHDRAW]" + req.toString());
+    	_logger.info("[ADMIN/PAY] " + req.toString());
     	String user_id = req.getParameter("id");
     	if (user_id == null || user_id == "null") user_id = "N/A";
     	/*double reward;
@@ -27,36 +27,36 @@ public class AdminServlet extends HttpServlet {
     	try {
     		reward = Double.parseDouble(req.getParameter("r")) / 100000000;	
     	} catch (NumberFormatException e) { reward = 0.00000000; }*/
-    	double r = 0.00000000;
-  		String username = "N/A";
-  		String email = "N/A";
-  		String address = "N/A";
-  		String delayed = "0.00000000";
-  		//double pending_tmp = 0.00000000;
-  		double total_tmp = 0.00000000;
-    	
+		double r = 0.00000000;
+		String username = "N/A";
+		String email = "N/A";
+		String address = "N/A";
+		String delayed = "0.00000000";
+		//double pending_tmp = 0.00000000;
+		double total_tmp = 0.00000000;
+		
 		DatastoreService db = DatastoreServiceFactory.getDatastoreService();
         Key userKey = KeyFactory.createKey("Users", user_id);
         try {
-	        Entity e = db.get(userKey);
-	  		username = e.getProperty("username").toString();
-	  		email = e.getProperty("email").toString();
-	  		address = e.getProperty("address").toString();
-	  		delayed = e.getProperty("delayed").toString();
-	  		r = Double.parseDouble(e.getProperty("pending").toString());
-	  		total_tmp = Double.parseDouble(e.getProperty("total").toString()) + r;
+			Entity e = db.get(userKey);
+			username = e.getProperty("username").toString();
+			email = e.getProperty("email").toString();
+			address = e.getProperty("address").toString();
+			delayed = e.getProperty("delayed").toString();
+			r = Double.parseDouble(e.getProperty("pending").toString());
+			total_tmp = Double.parseDouble(e.getProperty("total").toString()) + r;
         } catch (EntityNotFoundException e) {
-       		System.out.println(e.toString());
+			System.out.println(e.toString());
         }
 	    Entity e = new Entity("Users", user_id);
 	    String pending = "0.00000000";
 	    String total = String.format("%.8f", total_tmp);
 	    e.setProperty("delayed", delayed);
-  		e.setProperty("username", username);
-  		e.setProperty("email", email);
-  		e.setProperty("address", address);
-  		e.setProperty("pending", pending);
-  		e.setProperty("total", total);
+		e.setProperty("username", username);
+		e.setProperty("email", email);
+		e.setProperty("address", address);
+		e.setProperty("pending", pending);
+		e.setProperty("total", total);
 		db.put(e);
 		resp.sendRedirect("/admin/admin.jsp");
     }
